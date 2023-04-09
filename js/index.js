d0c7ce92fc0fc8ed4312c0a92b8a45480c0c7702
Nurseit <nurseitbakytbekov@gmail.com>
2023-04-09 23:32:22 +0600

    add Subscribe section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
-function swap_left(){
 
-}
-function swap_right(){
+const leftButton = document.querySelector('.swap__left');
+const rightButton = document.querySelector('.swap__right');
+rightButton.style.transform="rotate(180deg)";
 
-}
+leftButton.addEventListener('mouseover', () => {
+  const image = leftButton.querySelector('img');
+  image.src = 'img/svg/right_vector.svg';
+});
+
+rightButton.addEventListener('mouseover', () => {
+  const image = rightButton.querySelector('img');
+  image.src = 'img/svg/right_vector.svg';
+});
+leftButton.addEventListener('mouseout', () => {
+    const image = leftButton.querySelector('img');
+    image.src = 'img/svg/left_vector.svg';
+
+  });
+  
+  rightButton.addEventListener('mouseout', () => {
+    const image = rightButton.querySelector('img');
+    image.src = 'img/svg/left_vector.svg';
+
+  });
